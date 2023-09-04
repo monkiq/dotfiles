c4e88de3d50577f04edfa5f528fff3769a5c7f19
monkiq <monkiq@163.com>
2023-09-04 11:40:20 +0800

    Update .config/wezterm/config/domains.lua

--- dot_config/wezterm/config/domains.lua
+++ dot_config/wezterm/config/domains.lua
@@ -1,18 +1,36 @@
 return {
-   -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-   ssh_domains = {},
+	-- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
 
-   -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
-   unix_domains = {},
+	ssh_domains = {
+		{
+			-- This name identifies the domain
+			name = "dev",
+			-- The hostname or address to connect to. Will be used to match settings
+			-- from your ssh config file
+			remote_address = "9.135.12.153",
+			-- The username to use on the remote host
+			username = "monkiqchen",
 
-   -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
-   wsl_domains = {
-      {
-         name = 'WSL:Ubuntu',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish' },
-      },
-   },
+			multiplexing = "None",
+
+			ssh_option = {
+				identityfile = "~/.ssh/id_rsa.pub",
+			},
+
+			default_prog = { "zsh" },
+		},
+	},
+	-- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
+	unix_domains = {},
+
+	-- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
+	wsl_domains = {
+		{
+			name = "WSL:Ubuntu",
+			distribution = "Ubuntu",
+			username = "kevin",
+			default_cwd = "/home/kevin",
+			default_prog = { "fish" },
+		},
+	},
 }
