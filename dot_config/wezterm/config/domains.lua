2f31fa75150d4776314cc7c831c60ccf2b52f890
monkiq <monkiq@163.com>
2023-09-16 20:22:00 +0800

    Update .config/wezterm/config/domains.lua

--- dot_config/wezterm/config/domains.lua
+++ dot_config/wezterm/config/domains.lua
@@ -1,25 +1,6 @@
 return {
 	-- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
 
-	ssh_domains = {
-		{
-			-- This name identifies the domain
-			name = "dev",
-			-- The hostname or address to connect to. Will be used to match settings
-			-- from your ssh config file
-			remote_address = "9.135.12.153",
-			-- The username to use on the remote host
-			username = "monkiqchen",
-
-			multiplexing = "None",
-
-			ssh_option = {
-				identityfile = "~/.ssh/id_rsa.pub",
-			},
-
-			default_prog = { "zsh" },
-		},
-	},
 	-- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
 	unix_domains = {},
 
