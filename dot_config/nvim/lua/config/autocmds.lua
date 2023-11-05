6073dddda4176536f752b1fa408edf22d6683a95
monkiq <monkiq@163.com>
2023-11-05 21:33:08 +0800

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function(event)
+    if event.operator == "y" and event.regname == "+" then
+      vim.fn.OSCYankRegister("+")
+    end
+    if event.operator == "d" and event.regname == "+" then
+      vim.fn.OSCYankRegister("+")
+    end
+  end,
+})
